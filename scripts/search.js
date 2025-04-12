5d996e22db6e5d7b9948caf8ed0f125f8289b113
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-04-12 20:25:41 -0300

    feat: add responsiveness to header

--- scripts/search.js
+++ scripts/search.js
@@ -1,27 +1,50 @@
-const searchInput = document.querySelector('[data-search="input"]')
-const searchAnswer = document.querySelector('[data-search="answer"]')
-const searchButton = document.querySelector('[data-search="button"]')
+/** @type {Array<Element>} */
+const searchContainers = Array.from(document.querySelectorAll('[data-search="container"]'))
 
-const search = () => {
-    const value = searchInput.value
-    
-    if(value.length > 0){
-        searchAnswer.classList.remove("--hidden")
-        searchAnswer.textContent = "Você buscou por: '" + value + "'"        
+class SearchInput {
+    /** @type {HTMLInputElement} */
+    #searchInput
+    /** @type {HTMLDivElement} */
+    #searchAnswer
+    /** @type {HTMLButtonElement} */
+    #searchButton
+
+    /** @param {Element} searchContainer*/
+    constructor(searchContainer){
+        this.#searchInput = searchContainer.querySelector('[data-search="input"]')
+        this.#searchAnswer = searchContainer.querySelector('[data-search="answer"]')
+        this.#searchButton = searchContainer.querySelector('[data-search="button"]')
     }
 
-    else {
-        searchAnswer.classList.add("--hidden")
+    #search() {
+        const value = this.#searchInput.value
+        
+        if(value.length > 0){
+            this.#searchAnswer.classList.remove("--hidden")
+            this.#searchAnswer.textContent = "Você buscou por: '" + value + "'"        
+        }
+    
+        else {
+            this.#searchAnswer.classList.add("--hidden")
+        }
+    }
+
+    initialize(){
+        this.#searchButton.addEventListener("click", () => {
+            this.#search()
+         })
+         this.#searchInput.addEventListener("keydown", (e) => {
+             if(e.code === "Enter") this.#search()
+         })
+         
+         this.#searchInput.addEventListener("input", (e) => {
+             // @ts-ignore
+             if(e.target.value === "" ) this.#searchAnswer.classList.add("--hidden")
+         })
     }
 }
 
-searchButton.addEventListener("click", () => {
-    search()
-})
-searchInput.addEventListener("keydown", (e) => {
-    if(e.code === "Enter") search()
-})
-
-searchInput.addEventListener("input", (e) => {
-    if(e.target.value === "") searchAnswer.classList.add("--hidden")
+searchContainers.forEach(searchContainer => {
+    const searchInput = new SearchInput(searchContainer)
+    searchInput.initialize()
 })
